cfb62be1ee366247371f92a2c4a6da68f9e5392b
SANIA <20p0245sania@gmail.com>
2025-03-08 11:58:32 +0530

    Create App.java

---
+++ sem6/DevOps/P2/App.java
@@ -0,0 +1,19 @@
+package cse.cambridge.second;
+import org.apache.commons.lang3.StringUtils;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String text="Hello world"; 
+    	String t="sania";
+        System.out.println( "Trimmed: " + StringUtils.trim(text) );
+        System.out.println("Is Empty: "+ StringUtils.isEmpty(""));
+        System.out.println("starts with: "+ StringUtils.startsWith(text,t));
+        System.out.println("ends with: "+ StringUtils.endsWith(text,t));
+        System.out.println("equals: "+ StringUtils.equals(text,t));
+    }
+}
